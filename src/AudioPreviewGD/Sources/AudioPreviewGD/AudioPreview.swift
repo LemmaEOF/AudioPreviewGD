91c25907ae8d9189afb06f67fd4751792c0888c2
Meredith Espinosa <6722691+LemmaEOF@users.noreply.github.com>
2024-06-23 00:29:44 -0700

    fix everything now that I have an actual functioning LSP

--- src/AudioPreviewGD/Sources/AudioPreviewGD/AudioPreview.swift
+++ src/AudioPreviewGD/Sources/AudioPreviewGD/AudioPreview.swift
@@ -3,9 +3,23 @@ import SwiftGodot
 @Godot(.tool)
 class AudioPreview: RefCounted {
     var samples: [(min: UInt8, max: UInt8)] = []
-    var length: Double
-    var version: Int
-
+    var length: Double = 0
+    var version: Int = 0
+    
+    required init() {
+        super.init()
+    }
+    
+    required init(nativeHandle: UnsafeRawPointer) {
+        super.init(nativeHandle: nativeHandle)
+    }
+    
+    init(samples: [(min: UInt8, max: UInt8)], length: Double) {
+        super.init()
+        self.samples = samples
+        self.length = length
+    }
+    
     @Callable func get_length() -> Double {
         return length
     }
@@ -15,16 +29,16 @@ class AudioPreview: RefCounted {
             return 0
         }
 
-        let slice_from: Int = GD.clampi(value: start / length * samples.count, min: 0, max: samples.count - 1)
-        let slice_to: Int = GD.clampi(value: end / length * samples.count, min: slice_from + 1, max: samples.count - 1)
+        let slice_from = GD.clampi(value: Int64(start / length * Double(samples.count)), min: 0, max: Int64(samples.count - 1))
+        let slice_to = GD.clampi(value: Int64(end / length * Double(samples.count)), min: slice_from + 1, max: Int64(samples.count - 1))
 
-        var max: UInt8 = samples[0].max
+        var maxVal: UInt8 = samples[0].max
 
         for entry in samples {
-            max = GD.maxi(max, entry.max)
+            maxVal = max(maxVal, entry.max)
         }
 
-        return (max / 255.0) * 2.0 - 1.0
+        return (Double(maxVal) / 255.0) * 2.0 - 1.0
     }
 
     @Callable func get_min(start: Double, end: Double) -> Double {
@@ -32,15 +46,15 @@ class AudioPreview: RefCounted {
             return 0
         }
 
-        let slice_from: Int = GD.clampi(value: start / length * samples.count, min: 0, max: samples.count - 1)
-        let slice_to: Int = GD.clampi(value: end / length * samples.count, min: slice_from + 1, max: samples.count - 1)
+        let slice_from = GD.clampi(value: Int64(start / length * Double(samples.count)), min: 0, max: Int64(samples.count - 1))
+        let slice_to = GD.clampi(value: Int64(end / length * Double(samples.count)), min: slice_from + 1, max: Int64(samples.count - 1))
 
-        var min: UInt8 = samples[0].min
+        var minVal: UInt8 = samples[0].min
 
         for entry in samples {
-            min = GD.maxi(min, entry.min)
+            minVal = min(minVal, entry.min)
         }
 
-        return (min / 255.0) * 2.0 - 1.0
+        return (Double(minVal) / 255.0) * 2.0 - 1.0
     }
 }
--- src/AudioPreviewGD/Sources/AudioPreviewGD/AudioPreviewGenerator.swift
+++ src/AudioPreviewGD/Sources/AudioPreviewGD/AudioPreviewGenerator.swift
@@ -4,51 +4,63 @@ import SwiftGodot
 //TODO: actually generate previews, not there quite yet wheeee
 @Godot(.tool)
 class AudioPreviewGenerator : Node {
+    //TODO: can I expose this to Godot with current SwiftGodot? not sure I can, but it's here just in case
+    static var shared: AudioPreviewGenerator = AudioPreviewGenerator()
+    
     class Preview {
         var preview: AudioPreview
         var baseStream: AudioStream
-        var playback: AudioPlayback
-        var generating: Bool
-        var id: UInt
+        var playback: AudioStreamPlayback
+        var generating: Bool = false
+        var id: UInt = 0
+        
+        init(preview: AudioPreview, baseStream: AudioStream, playback: AudioStreamPlayback, generating: Bool, id: UInt) {
+            self.preview = preview
+            self.baseStream = baseStream
+            self.playback = playback
+            self.generating = generating
+            self.id = id
+        }
     }
 
-    #signal("preview_updated", argument: ["audio_stream": AudioStream.self])
+    #signal("preview_updated", arguments: ["audio_stream": AudioStream.self])
 
     static var previews: [UInt: Preview] = [:]
 
-    @Callable static func generate_preview(stream: AudioStream) -> AudioPreview {
+    //TODO: @Callable doesn't work for static funcs yet, so just leave it like this and access static previews dict
+    @Callable func generate_preview(stream: AudioStream) -> AudioPreview {
         var streamId = stream.getInstanceId()
 
-        if previews.keys.contains(streamId) {
-            return previews[streamId].preview
+        if AudioPreviewGenerator.previews.keys.contains(streamId) {
+            // ! is here because we just checked existence
+            return AudioPreviewGenerator.previews[streamId]!.preview
         }
 
-        var preview = Preview()
-        preview.baseStream = stream
-        preview.id = streamId
-        preview.playback = stream.instantiatePlayback()
-        preview.generating = true
+        var baseStream = stream
+        var id = streamId
+        var playback = stream.instantiatePlayback()!
 
         var length = stream.getLength()
         if (length == 0) {
             length = 60 * 5 //5 minutes default
         }
 
-        var frames: Int = (AudioServer.GetMixRate() * length) / 20
+        var frames: Int = Int(AudioServer.getMixRate() * length) / 20
 
         var samples: [(min: UInt8, max: UInt8)] = []
 
-        for i in 0..frames {
+        for i in 0...frames {
             samples[i] = (0, 255) //this is 127 in the original C++, even though it's an unsigned int??
         }
 
-        var audioPreview = AudioPreview()
+        var audioPreview = AudioPreview(samples: samples, length: length)
         audioPreview.samples = samples
         audioPreview.length = length
-        preview.preview = audioPreview
+        
+        var preview = Preview(preview: audioPreview, baseStream: baseStream, playback: playback, generating: true, id: id)
 
-        previews[streamId] = preview
+        AudioPreviewGenerator.previews[streamId] = preview
 
-        return preview
+        return preview.preview
     }
 }
